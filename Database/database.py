7a20bb57ac9e1a39c9b164f9f70958e1e09303c8
TaiSewell <Tai.sewell1@gmail.com>
2024-07-30 17:10:03 -0500

    Added URL to the database

--- Database/database.py
+++ Database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = ''
+URL_DATABASE = 'mysql+pymysql://root:R81d3r$dbr$@localhost:3306/ToDoList_Application'
 
 engine = create_engine(URL_DATABASE)
 
