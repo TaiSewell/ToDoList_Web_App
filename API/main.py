aa82be8ce0a81cce093ddba27d4b4a6d484a91ba
TaiSewell <Tai.sewell1@gmail.com>
2024-07-30 14:40:56 -0500

    Added a pydantic model to hold the task information

--- API/main.py
+++ API/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+
 
 tdlApp = FastAPI()
 
+class Task(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+
+TASKS = []
 
 @tdlApp.get("/")
 def read_api():
-    return {'Home': 'Page'}
+    return TASKS
+
+
+tdlApp.post("/")
+def create_task(task: Task):
+    TASKS.append(task)
+    return task
