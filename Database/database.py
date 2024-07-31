cd29c7b4acda2c0495b159ac17fd20f2ad08fdfd
TaiSewell <Tai.sewell1@gmail.com>
2024-07-30 19:14:21 -0500

    Cleared files to redo them

--- Database/database.py
+++ Database/database.py
@@ -1,11 +1,1 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:R81d3r$dbr$@localhost:3306/ToDoList_Application'
-
-engine = create_engine(URL_DATABASE)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
