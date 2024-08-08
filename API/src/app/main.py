c5426a14a1fbe21029649e31a4821ef0a0e8cd41
TaiSewell <Tai.sewell1@gmail.com>
2024-08-08 18:36:44 -0500

    ADded get route for list_id

--- API/src/app/main.py
+++ API/src/app/main.py
@@ -4,7 +4,11 @@ tdlapp = FastAPI()
 
 @tdlapp.get("/")
 def read_root():
-    return {"message": "Hello, Tai!!"}
+    return {"Hello": "World"}
+
+@tdlapp.get("/lists/{list_id}")
+def read_list(list_id: int, q: str = None):
+    return {"list_id": list_id, "q": q}
 
 
 
