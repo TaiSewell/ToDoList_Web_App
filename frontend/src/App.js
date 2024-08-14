5fe29f8e0513794f92320416c6d183dac57f5dd6
TaiSewell <Tai.sewell1@gmail.com>
2024-08-14 15:52:32 -0500

    New File Tracking On Frontend
    These files are now being tracked. I also updated the .gitignore file.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a starter for your frontend.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
