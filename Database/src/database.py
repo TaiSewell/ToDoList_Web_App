97fa6ce24c0a5a096f186044b35ec0c7c2a65ddd
Tai Sewell <89483418+TaiSewell@users.noreply.github.com>
2025-01-09 15:59:14 -0600

    Updated the structure of the project and created the correct files needed for each service

--- Database/src/database.py
+++ Database/src/database.py
@@ -1,1 +1,35 @@
+"""
+***********************************************
+Developer: Tai Sewell
 
+File: database.py
+
+Description: This class is used to create the 
+session that the database will be running on.
+***********************************************
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = "mysql+pymysql://root:password@db/todo_app"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+"""
+***********************************************
+Method: User(Base)
+
+Description: This method creates a db session
+and then closes the db session when prompted to.
+***********************************************
+"""
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
