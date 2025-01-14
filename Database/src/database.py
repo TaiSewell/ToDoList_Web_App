63083a4d04a1347a579a55a578f0ad2934360a49
Tai Sewell <89483418+TaiSewell@users.noreply.github.com>
2025-01-14 16:46:30 -0600

    Updated the database.py file to use pymysql driver instead mysqldb

--- Database/src/database.py
+++ Database/src/database.py
@@ -15,28 +15,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
-
-# Access database connection variables from environment
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Ensure DATABASE_URL is set
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not found in environment variables")
 
 print(f"Connecting to MySQL database with URL: {DATABASE_URL}")
 
-# SQLAlchemy engine for connecting to the database
-engine = create_engine(DATABASE_URL)
 
-# Declarative Base for defining models
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 Base = declarative_base()
-
-# Create a session factory for making database connections
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Dependency for FastAPI routes
 def get_db():
     db = SessionLocal()
     try:
