103cb0b0a43bb93f66b0b00fd3d05598adc3a2c7
Tai Sewell <89483418+TaiSewell@users.noreply.github.com>
2025-01-15 18:22:23 -0600

    I have now added all the methods/endpoints for the API that I will need and they are working and interacting well. The documentation also has been applied to the methods and files used

--- Database/src/models.py
+++ Database/src/models.py
@@ -24,10 +24,10 @@ table for users.
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True, nullable=False)  # Specify length for VARCHAR
-    hashed_password = Column(String(100), nullable=False)                   # Specify length for VARCHAR
+    username = Column(String(50), unique=True, index=True, nullable=False)  
+    hashed_password = Column(String(100), nullable=False)                  
 
-    tasks = relationship("Task", back_populates="owner")
+    tasks = relationship("Task", back_populates="owner", cascade="all, delete-orphan")
 
 """
 ***********************************************
@@ -40,8 +40,8 @@ table for tasks.
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), nullable=False)                             # Specify length for VARCHAR
-    description = Column(String(255))                                       # Specify length for VARCHAR
+    title = Column(String(100), nullable=False)                             
+    description = Column(String(255))                                       
     owner_id = Column(Integer, ForeignKey('users.id'))
 
     owner = relationship("User", back_populates="tasks")
